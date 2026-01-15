5eaaf651f1d1b686a31825783d4852fd79e371de
Skaro7 <marc_tau72@hotmail.com>
2026-01-15 13:36:27 +0100

    feat: filter brands with 'o' and >5 letters

---
+++ src/Exercise2/ReturnO5PlusLeters.java
@@ -0,0 +1,15 @@
+package Exercise2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReturnO5PlusLeters {
+    public static void main(String[] args) {
+        List<String> carBrands = Arrays.asList("Toyota", "Ford", "Volvo", "Hyundai", "Opel");
+        List<String> result = carBrands.stream()
+                .filter(brand -> brand.contains("o") && brand.length() > 5)
+                .collect(Collectors.toList());
+        System.out.println(result);
+    }
+}
