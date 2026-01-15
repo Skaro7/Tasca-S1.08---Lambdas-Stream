168d3e7caa2b4a65697069aa0368f8359c19dfb9
Skaro7 <marc_tau72@hotmail.com>
2026-01-15 13:35:45 +0100

    feat: add months list printer with lambda

---
+++ src/Exercise3/MonthsList.java
@@ -0,0 +1,15 @@
+package Exercise3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MonthsList {
+    public static void main(String[] args) {
+        List<String> months = Arrays.asList("January", "February", "March", "April", "May",
+                "June", "July", "August", "September", "October",
+                "November", "December");
+        Consumer<String> printer = month -> System.out.println(month);
+        months.forEach(printer);
+    }
+}
