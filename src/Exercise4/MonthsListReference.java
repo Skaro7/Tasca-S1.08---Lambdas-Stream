399c9de135157e6c3b5c9b95199c2a83ebf50703
Skaro7 <marc_tau72@hotmail.com>
2026-01-16 11:50:29 +0100

    feat: print list using method reference

---
+++ src/Exercise4/MonthsListReference.java
@@ -0,0 +1,15 @@
+package Exercise4;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MonthsListReference {
+    public static void main(String[] args) {
+        List<String> months = Arrays.asList("January", "February", "March", "April", "May",
+                "June", "July", "August", "September", "October",
+                "November", "December");
+        Consumer<String> printer = System.out::println;
+        months.forEach(printer);
+    }
+}
