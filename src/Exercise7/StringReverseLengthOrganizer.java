e22715b5f7bbf68ff9a8cedcf834c0e995336684
Skaro7 <marc_tau72@hotmail.com>
2026-01-22 11:05:06 +0100

    fix: refactor to scope of exercise.

--- src/Exercise7/StringReverseLengthOrganizer.java
+++ src/Exercise7/StringReverseLengthOrganizer.java
@@ -1,17 +1,26 @@
 package Exercise7;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringReverseLengthOrganizer {
     public static void main(String[] args) {
         List<Object> mixedList = Arrays.asList("fg43qtfqwrfq3", "wejmoiifrwepmfwe", "317uu803147803248032", "432", "432432");
         mixedList.sort((a, b) -> {
             if (a instanceof String && b instanceof String) {
-                return Integer.compare(((String) b).length(), ((String) a).length());
+                return Integer.compare(((String) a).length(), ((String) b).length());
             }
             return 0;
         });
-        System.out.println(mixedList);
+
+        List<String> result = mixedList.stream()
+                .filter(obj -> obj instanceof String)
+                .map(obj -> (String) obj)
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .collect(Collectors.toList());
+
+        System.out.println(result);
     }
 }
