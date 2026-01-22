9f39bb9085848db7dae5a778b3161abd53398412
Skaro7 <marc_tau72@hotmail.com>
2026-01-22 11:03:03 +0100

    fix: changed to comparator.

--- src/Exercise6/StringLengthOrganizer.java
+++ src/Exercise6/StringLengthOrganizer.java
@@ -1,6 +1,7 @@
 package Exercise6;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,7 +18,7 @@ public class StringLengthOrganizer {
         List<String> result = mixedList.stream()
                 .filter(obj -> obj instanceof String)
                 .map(obj -> (String) obj)
-                .sorted((s1, s2) -> s1.length() - s2.length())
+                .sorted(Comparator.comparingInt(String::length))
                 .collect(Collectors.toList());
 
         System.out.println(result);
